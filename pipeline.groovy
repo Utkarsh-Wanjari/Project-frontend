b73ea2cdbfc2a3fe2224b680118ad4aab1597812
Utkarsh_Wanjari <utkarshwanjari13@gmail.com>
2025-08-21 15:38:29 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages{
         stage('code-pull'){
             steps {
-                git branch: 'dev', url: 'https://github.com/mayurmwagh/Project-frontend.git'
+                git branch: 'dev', url: 'https://github.com/Utkarsh-Wanjari/Project-frontend.git'
             }
         }
         stage('code-build'){
@@ -18,7 +18,7 @@ pipeline {
             steps {
                 withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
                   sh '''
-                      aws s3 cp --recursive dist/angular-frontend s3://cbz-frontend-project-bux123-king/
+                      aws s3 cp --recursive dist/angular-frontend s3://cbz-frontend-project-bux123-queen/
                  '''
 }
                 
