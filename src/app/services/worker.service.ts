a25b3e46f12211598c6b564edd700c9375d10cea
Utkarsh_Wanjari <utkarshwanjari13@gmail.com>
2025-08-21 15:57:40 +0530

    Update worker.service.ts

--- src/app/services/worker.service.ts
+++ src/app/services/worker.service.ts
@@ -9,7 +9,7 @@ import { Worker } from '../models/worker';
 })
 export class WorkerService {
 
-  private getUrl: string = "http://a1897db23a8d04d7687b9bca23cbd871-1088263503.us-east-2.elb.amazonaws.com:8080/api/v1/workers";
+  private getUrl: string = "http://ad2b5040a63774492ba8620afc1a935d-1665929383.us-east-2.elb.amazonaws.com:8080/api/v1/workers";
 
   constructor(private _httpClient: HttpClient) { }
 
